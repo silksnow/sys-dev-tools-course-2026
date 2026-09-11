288ef2a9131f2a195f2a5d1c0aaecf99997fb9fa
silksnow <tangcwyx@163.com>
2026-09-11 20:12:11 +0800

    feat: 第三周 q09 从源码构建并在干净环境安装 Wheel

---
+++ week3/q09/src/greetlab/cli.py
@@ -0,0 +1,8 @@
+# src/greetlab/cli.py
+import argparse
+
+def main():
+    p = argparse.ArgumentParser()          # 解析器
+    p.add_argument("--name", required=True)  # 必填 --name 参数
+    a = p.parse_args()                     # 解析命令行，返回命名空间
+    print(f"Hello, {a.name}!")             # f-string 插值输出
